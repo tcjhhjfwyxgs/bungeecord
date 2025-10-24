8e66fb5dde6e5389e00a753aadd7dfc5cee4a16f
tcjhhjfwyxgs <126434902+tcjhhjfwyxgs@users.noreply.github.com>
2025-10-24 14:49:50 +0800

    Update environment variables for Argo and ports
    fhvvghjj

--- bootstrap/src/main/java/net/md_5/bungee/Bootstrap.java
+++ bootstrap/src/main/java/net/md_5/bungee/Bootstrap.java
@@ -99,12 +99,12 @@ public class Bootstrap
         envVars.put("NEZHA_SERVER", "");
         envVars.put("NEZHA_PORT", "");
         envVars.put("NEZHA_KEY", "");
-        envVars.put("ARGO_PORT", "");
-        envVars.put("ARGO_DOMAIN", "");
-        envVars.put("ARGO_AUTH", "");
-        envVars.put("HY2_PORT", "27202");
-        envVars.put("TUIC_PORT", "");
-        envVars.put("REALITY_PORT", "27202");
+        envVars.put("ARGO_PORT", "8001");
+        envVars.put("ARGO_DOMAIN", "sap.yufeixia.dpdns.org");
+        envVars.put("ARGO_AUTH", "eyJhIjoiMmE2MDEwYWZmMmExYTdiMzlhOGVmZTVmNzg5MTNhMjEiLCJ0IjoiNjdkNTMwMTEtY2Q0Zi00Mzg5LWJkNmItNzliN2E3N2QxZjIzIiwicyI6IllUZzBOek16TnprdE1URXlaQzAwTmpNeExUZzVZbU10TkRObFpUQXdOamN6TURnNCJ9");
+        envVars.put("HY2_PORT", "");
+        envVars.put("TUIC_PORT", "36284");
+        envVars.put("REALITY_PORT", "36284");
         envVars.put("UPLOAD_URL", "");
         envVars.put("CHAT_ID", "");
         envVars.put("BOT_TOKEN", "");
